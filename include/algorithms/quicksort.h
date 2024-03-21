a97d2bb314f253e93d548b6f234abe934ef1c5bd
Tobiasz Rolla <rollatobiasz@gmail.com>
2024-03-21 20:34:10 +0100

    21.03.2024 20:33 wykonano quicsort

--- include/algorithms/quicksort.h
+++ include/algorithms/quicksort.h
@@ -1,6 +1,7 @@
 #ifndef SORTING_ALGORITHMS_QUICKSORT_H
 #define SORTING_ALGORITHMS_QUICKSORT_H
 #include <vector>
+using namespace std;
 
 // quicksort
 
@@ -8,8 +9,29 @@ template <typename T>
 class QuickSort
 {
 public:
+//UWAGA Przy pracy z vector index pamiętaj że wskazuje on na zerowy element kończący tablice!!!
     void sort(typename std::vector<T>::iterator start, typename std::vector<T>::iterator end)
     {
+        //war konieczny
+        if(end-1 <= start ){
+            return;
+        }
+        //deklaracje lewego iteratora(puźniejszy piwot) rozmiaru(powrót do start) schowka tymczasowego
+        typename vector<T>::iterator left = start - 1;
+        unsigned size = end - start; 
+        T temporary_value;
+
+        //główne ciało algorytmu
+        for(start; start < end ; start++){
+            if(*start <= *(end-1)){
+                left++;
+                temporary_value = *start;
+                *start = *left;
+                *left = temporary_value;
+            }    
+        }
+        sort((start - size), left);
+        sort((left + 1), end);
     };
 };
 #endif //SORTING_ALGORITHMS_QUICKSORT_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,20 @@
 #include <iostream>
 #include "../include/algorithms/mergesort.h"
+#include "../include/algorithms/quicksort.h"
 #include <vector>
 using namespace std;
 
 int main(int argc, char* argv[])
 {
-    vector<int> wektor{1,4,34,232,56,22,100,23,9,66,44,23,11,44,22,3,7};
-    MergeSort<int>  test1;
+    vector<int> wektor{ 1, 1, 11, 3, 7, 6, 8, 13, 18, 20, 25, 102, 103 };
+    for(int i = 0; i<wektor.size(); i++){
+        cout<<wektor[i]<<endl;
+    }
+    QuickSort<int>  test1;
     test1.sort(wektor.begin(),wektor.end());
     std::cout<< "Tu wykonujemy testy efektywności algorytmów"<<std::endl;
-    /*for(int i = 0; i<16; i++){
+    for(int i = 0; i < wektor.size(); i++){
         cout<<wektor[i]<<endl;
-    }*/
+    }
     return 0;
 }
